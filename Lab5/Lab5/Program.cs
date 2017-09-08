6b3db3c5c6c6ef95dc3b55b85cdcaf0ff0938651
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 11:22:39 +0700

    W05P08

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -6,19 +6,19 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-        line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-        line10:
-            Console.WriteLine("Line 10");
+            int a = 0;
+            int b = 10;
+
+            try
+            {
+                b /= a;
+            }
+
+            catch(DivideByZeroException )
+
+            {
+                Console.WriteLine(a);
+            }
 
         }
     }
