9c12cee592e6d84edd7119ad94f74403d5c7851c
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 11:09:08 +0700

    W05P07

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -6,19 +6,10 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-        line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-        line10:
-            Console.WriteLine("Line 10");
+            int a = int.MaxValue;
+            a *= 2;
+            Console.WriteLine(a);
+
 
         }
     }
